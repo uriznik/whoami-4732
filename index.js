74da6edd4fae0115f249cc311f0ab677b4992193
uriznik <uriznik@users.noreply.github.com>
2017-01-02 16:31:59 +1300

    fix error

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
 	var who = {
-		ipaddress: req.headers.x-forwarded-for || req.ip,
-		language: req.headers.accept-language,
-		software: req.headers.user-agent
+		ipaddress: req.headers["x-forwarded-for"] || req.ip,
+		language: req.headers["accept-language"],
+		software: req.headers["user-agent"]
 	}
 	res.send(who);
 });
