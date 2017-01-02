2a72db9abd5112e0ec061d3cd8ab18281d2d0532
uriznik <uriznik@users.noreply.github.com>
2017-01-02 16:21:52 +1300

    fetch specific headers

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
 	var who = {
-		ipaddress: req.ip,
+		ipaddress: req.headers.x-forwarded-for || req.ip,
+		language: req.headers.accept-language,
+		software: req.headers.user-agent
 	}
-	res.send(req.headers);
+	res.send(who);
 });
 
 app.listen(port);
