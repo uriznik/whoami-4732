136a2747ecb98524495bb8dd02847f3fd9d514ff
uriznik <uriznik@users.noreply.github.com>
2017-01-02 19:27:44 +1300

    extract software and language from headers

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
+var uaSoftware = /^[^\s]*\s\(([^)]*)\)/;
+
 app.get('/', function (req, res) {
+	var language = req.headers["accept-language"];
+	var ua = req.headers["user-agent"];
+
+	if (language && language.indexOf(',') > 0) {
+		language = language.split(',')[0];
+	}
+	
+	if (ua) {
+		var match = uaSoftware.exec(ua);
+		var software = match ? match[1] : "";
+	}
 	var who = {
 		ipaddress: req.headers["x-forwarded-for"] || req.ip,
-		language: req.headers["accept-language"],
-		software: req.headers["user-agent"]
+		language: language,
+		software: software
 	}
+
 	res.send(who);
 });
 
